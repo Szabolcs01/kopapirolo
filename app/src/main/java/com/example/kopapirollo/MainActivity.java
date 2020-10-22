76f2df9f67dd927652c219c1615d06bd60c353fc
Szabolcs01 <oszabi000@gmail.com>
2020-10-22 17:51:47 +0200

    AZ eredmények hozzá adása

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     ImageView im_my,im_cup;
     String svalasztas, cvalasztas, result;
     Random r;
+    TextView computerEredmeny,emberEredmeny;
+    int computerPont = 0;
+    int emberPont = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,8 @@ im_cup=(ImageView) findViewById(R.id.im_cup);
         b_papper=(Button) findViewById(R.id.b_papper);
         b_scissors=(Button) findViewById(R.id.b_scissors);
 r=new Random();
+        TextView embipont = findViewById(R.id.emberEredmeny);
+        TextView compipont = findViewById(R.id.computerEredmeny);
 
         b_rock.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,22 +74,34 @@ else if(cup==2){
         }
 if (svalasztas.equals("Rock") && cvalasztas.equals("Papper")){
     result="You Lose";
+    computerPont++;
+    computerEredmeny.setText(String.valueOf(computerPont));
 }else
         if (svalasztas.equals("Rock") && cvalasztas.equals("Scissors")){
             result="You Win";
+            emberPont++;
+            emberEredmeny.setText(String.valueOf(computerPont));
         }
         else
         if (svalasztas.equals("Scissors") && cvalasztas.equals("Papper")){
             result="You Win";
+            emberPont++;
+            emberEredmeny.setText(String.valueOf(computerPont));
         }else
         if (svalasztas.equals("Scissors") && cvalasztas.equals("Rock")){
             result="You Lose";
+            computerPont++;
+            computerEredmeny.setText(String.valueOf(computerPont));
         }else
         if (svalasztas.equals("Papper") && cvalasztas.equals("Scissors")){
             result="You Lose";
+            computerPont++;
+            computerEredmeny.setText(String.valueOf(computerPont));
         }else
         if (svalasztas.equals("Papper") && cvalasztas.equals("Rock")){
             result="You Win";
+            emberPont++;
+            emberEredmeny.setText(String.valueOf(computerPont));
         }else
         if (svalasztas.equals("Scissors") && cvalasztas.equals("Scissors")){
             result="Draw";
