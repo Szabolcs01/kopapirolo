58a0feb892837734d7ec165fec220d6689081d97
Szabolcs01 <oszabi000@gmail.com>
2020-10-22 14:20:16 +0200

    Ki nézett meg csinálása és toust meg a játék menet

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -3,12 +3,98 @@ package com.example.kopapirollo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b_rock, b_papper, b_scissors;
+    ImageView im_my,im_cup;
+    String svalasztas, cvalasztas, result;
+    Random r;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+im_cup=(ImageView) findViewById(R.id.im_cup);
+        im_my=(ImageView) findViewById(R.id.im_my);
+        b_rock=(Button) findViewById(R.id.b_rock);
+        b_papper=(Button) findViewById(R.id.b_papper);
+        b_scissors=(Button) findViewById(R.id.b_scissors);
+r=new Random();
+
+        b_rock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               svalasztas="Rock";
+                im_cup.setImageResource(R.drawable.rock);
+            }
+        });
+        b_rock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                svalasztas="Papper";
+                im_cup.setImageResource(R.drawable.paper);
+            }
+        });
+        b_rock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                svalasztas="Scissors";
+                im_cup.setImageResource(R.drawable.scissors);
+            }
+        });
+
+
+
+    }
+
+    public void calculate(){
+int cup=r.nextInt(3);
+if (cup==0){
+    cvalasztas="Rock";
+
+}else if(cup==1){
+            cvalasztas="Papper";
+
+}
+else if(cup==2){
+            cvalasztas="Scissors";
+
+        }
+if (svalasztas.equals("Rock") && cvalasztas.equals("Papper")){
+    result="You Lose";
+}else
+        if (svalasztas.equals("Rock") && cvalasztas.equals("Scissors")){
+            result="You Win";
+        }
+        else
+        if (svalasztas.equals("Scissors") && cvalasztas.equals("Papper")){
+            result="You Win";
+        }else
+        if (svalasztas.equals("Scissors") && cvalasztas.equals("Rock")){
+            result="You Lose";
+        }else
+        if (svalasztas.equals("Papper") && cvalasztas.equals("Scissors")){
+            result="You Lose";
+        }else
+        if (svalasztas.equals("Papper") && cvalasztas.equals("Rock")){
+            result="You Win";
+        }else
+        if (svalasztas.equals("Scissors") && cvalasztas.equals("Scissors")){
+            result="Draw";
+        }else
+        if (svalasztas.equals("Papper") && cvalasztas.equals("Papper")){
+            result="Draw";
+        }else
+        if (svalasztas.equals("Rock") && cvalasztas.equals("Rock")){
+            result="Draw";
+        }
+
+        Toast.makeText(MainActivity.this,result,Toast.LENGTH_SHORT).show();
     }
 }
