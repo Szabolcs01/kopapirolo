3e7be6f61a411b653fc57d96da6d0a11e8ddce10
Szabolcs01 <oszabi000@gmail.com>
2020-10-23 17:23:12 +0200

    Új kinézett és döntetlen számolása

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -19,9 +19,10 @@ public class MainActivity extends AppCompatActivity {
     ImageView im_my, im_cup;
     String svalasztas, cvalasztas, result;
     Random r;
-    TextView computerEredmeny, emberEredmeny;
+    TextView computerEredmeny, emberEredmeny,dontetlen;
     int computerPont = 0;
     int emberPont = 0;
+    int dontetlenPont=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
         b_papper = (Button) findViewById(R.id.b_papper);
         b_scissors = (Button) findViewById(R.id.b_scissors);
         r = new Random();
-        TextView embipont = findViewById(R.id.emberEredmeny);
-        TextView compipont = findViewById(R.id.computerEredmeny);
-
+        TextView emberPont = findViewById(R.id.emberEredmeny);
+        TextView computerPont = findViewById(R.id.computerEredmeny);
+        TextView dontetlenPont = findViewById(R.id.dontetlen);
         b_rock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,12 +84,12 @@ public class MainActivity extends AppCompatActivity {
         } else if (svalasztas.equals("Rock") && cvalasztas.equals("Scissors")) {
             result = "You Win";
             emberPont++;
-            emberEredmeny.setText(String.valueOf(computerPont));
+            emberEredmeny.setText(String.valueOf(emberEredmeny));
             jatekVege();
         } else if (svalasztas.equals("Scissors") && cvalasztas.equals("Papper")) {
             result = "You Win";
             emberPont++;
-            emberEredmeny.setText(String.valueOf(computerPont));
+            emberEredmeny.setText(String.valueOf(emberEredmeny));
             jatekVege();
         } else if (svalasztas.equals("Scissors") && cvalasztas.equals("Rock")) {
             result = "You Lose";
@@ -103,14 +104,20 @@ public class MainActivity extends AppCompatActivity {
         } else if (svalasztas.equals("Papper") && cvalasztas.equals("Rock")) {
             result = "You Win";
             emberPont++;
-            emberEredmeny.setText(String.valueOf(computerPont));
+            emberEredmeny.setText(String.valueOf(emberEredmeny));
             jatekVege();
         } else if (svalasztas.equals("Scissors") && cvalasztas.equals("Scissors")) {
             result = "Draw";
+            dontetlenPont++;
+            dontetlen.setText(String.valueOf(dontetlen));
         } else if (svalasztas.equals("Papper") && cvalasztas.equals("Papper")) {
             result = "Draw";
+            dontetlenPont++;
+            dontetlen.setText(String.valueOf(dontetlen));
         } else if (svalasztas.equals("Rock") && cvalasztas.equals("Rock")) {
             result = "Draw";
+            dontetlenPont++;
+            dontetlen.setText(String.valueOf(dontetlen));
         }
 
         Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
