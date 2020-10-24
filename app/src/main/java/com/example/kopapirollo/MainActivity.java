12ff3e5d4ca2d360165a236861dc733cae80dd21
Szabolcs01 <oszabi000@gmail.com>
2020-10-24 13:20:21 +0200

    Calculate és játék vége hozzáadása

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -42,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 svalasztas = "Rock";
                 im_cup.setImageResource(R.drawable.rock);
+                calculate();
+                jatekVege();
             }
         });
         b_rock.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 svalasztas = "Papper";
                 im_cup.setImageResource(R.drawable.paper);
+                calculate();
+                jatekVege();
             }
         });
         b_rock.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 svalasztas = "Scissors";
                 im_cup.setImageResource(R.drawable.scissors);
+                calculate();
+                jatekVege();
             }
         })
 
